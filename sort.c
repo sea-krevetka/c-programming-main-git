cd07d8576a9c3bbc7ed821238bd4c6aeb74d1637
sea_krevetka <bagasiyvika@gmail.com>
2025-03-11 06:38:02 +0700

    added file mergesort

---
+++ sort.c
@@ -0,0 +1,50 @@
+void stalin_sort(int* numbers, int* size) {
+    int i = 0;
+    int maximal = numbers[0];
+    int* gulag = (int*)malloc(*size * sizeof(int)); //creating array
+    int gulag_size = 0;
+
+    while (i < *size) {
+        int element = numbers[i];
+        if (maximal > element) {
+            printf("Element %d sent to Gulag\n", element);
+            gulag[gulag_size++] = element;
+            for (int j = i; j < *size - 1; j++) {
+                numbers[j] = numbers[j + 1];
+            }
+            (*size)--;
+        } else {
+            maximal = element;
+            i++;
+        }
+    }
+
+    printf("Numbers: ");
+    for (i = 0; i < *size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n"); //yay its a comment!!
+
+    printf("Gulag: ");
+    for (i = 0; i < gulag_size; i++) {
+        printf("%d ", gulag[i]);
+    }
+    printf("\n");
+
+    free(gulag);
+}
+
+int main() { //added main
+    int numbers[] = {5, 3, 8, 6, 7, 2, 42, 38};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    printf("Original Numbers: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", numbers[i]);
+    }
+    printf("\n");
+
+    stalin_sort(numbers, &size);
+
+    return 0;
+}
